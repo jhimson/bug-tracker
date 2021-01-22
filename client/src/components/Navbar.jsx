7d4a76f3f5f054751f8694c4477b41734707e90f
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-21 21:24:00 -0500

    Updated navbar design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import LOGO from '../assets/images/j•p.svg'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiOutlineClose} from 'react-icons/ai'
+// import {Link as ScrollLink} from 'react-scroll'
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     return(
-        <div className='flex'>
-            <h1>Navbar</h1>
-        </div>
+        //! Main navbar wrapper ~~~START
+        <div className='flex flex-col justify-between py-3 mx-auto bg-black lg:flex-row lg:p-0 navbar-container'>
+            <div className="flex items-center justify-between lg:mx-21">
+                <div className="flex cursor-pointer lg:animate-float">
+                    <img src={LOGO} alt="" className='w-24 h-24 lg:h-32 lg:w-32'/>
+                </div>
+                <div className={`${toggle ? 'flex' : 'hidden'} flex-col justify-center lg:items-center pb-4 lg:pb-0 lg:flex`}>
+                <nav>
+                    <ul className="flex flex-col gap-5 px-8 text-white lg:flex-row">
+                        {/* <li><ScrollLink to='projects' smooth={true}><a href="" className='nav-item'>Projects</a></ScrollLink></li> */}
+                        <li><a href="" className='text-2xl nav-item'>Dashboard</a></li>
+                        <li><a href="" className='text-2xl nav-item'>Projects</a></li>
+                    </ul>
+                </nav>
+                </div>
+                <div className="flex mr-4 lg:hidden">
+                    {toggle ? <AiOutlineClose size='3em' className='font-bold text-red-600 cursor-pointer'onClick={() => setToggle(!toggle)}/> : <GiHamburgerMenu size='3em' className='font-extrabold text-white cursor-pointer' onClick={() => setToggle(!toggle)}/> }
+                </div>
+            </div>
+
+            <div className={`${toggle ? 'flex' : 'hidden'} flex-col justify-center lg:items-center pb-4 lg:pb-0 lg:flex`}>
+                <nav>
+                    <ul className="flex flex-col px-8 text-white lg:flex-row">
+                        {/* <li><ScrollLink to='projects' smooth={true}><a href="" className='nav-item'>Projects</a></ScrollLink></li> */}
+                        <li><a href="" className='nav-item'>Skills</a></li>
+                        <li><a href="" className='nav-item'>Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div> //! Main navbar wrapper ~~~END
     )
 }
 
