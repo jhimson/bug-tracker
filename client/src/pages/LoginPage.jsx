c03614abf8daf588bb38769abf6a32c27fd910df
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-21 20:23:12 -0500

    Created LoginPage

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1 className='text-2xl'>Login page</h1>
+        </div>
+    )
+}
+export default LoginPage
