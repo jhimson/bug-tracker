cb97e48c1d600cdafd5a80fe6a0ba74ef7d6c980
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 10:59:47 -0500

    Created RegisterPage and made some initial designs

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+import logo from '../assets/images/login-logo.png'
+
+
+
+const RegisterPage = () => {
+    return (
+            <Layout>
+                <div className='flex items-center justify-center h-screen'>
+                    <div className='w-1/4 h-auto p-5 bg-gray-200 rounded-lg'>
+                        <div>
+                            <img src={logo} alt="" className='w-full h-72'/>
+                        </div>
+                        <form className='space-y-2'>
+                            <h1 className='text-2xl font-bold text-center'>Account Register</h1>
+                            <div>
+                                <label htmlFor="firstname" className='font-semibold'>Firstname:</label>
+                                <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                            </div>
+                            <div>
+                                <label htmlFor="lastname" className='font-semibold'>Lastname:</label>
+                                <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                            </div>
+                            <div>
+                                <label htmlFor="email" className='font-semibold'>Email:</label>
+                                <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                            </div>
+                            <div>
+                                <label htmlFor="password" className='font-semibold'>Password:</label>
+                                <input type="password" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                            </div>
+                            <div>
+                                <button className='w-full py-2 mt-5 text-lg font-bold bg-blue-400 rounded focus:outline-none focus:ring-2 '>Sign up</button>
+                            </div>
+                            <div>
+                                <p className='mt-5 text-center text-md'>Already have an account? <a href="" className='text-purple-600'>Sign in</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </Layout>
+    )
+}
+
+export default RegisterPage;
