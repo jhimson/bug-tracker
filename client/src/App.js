c0a42b612a01b744a7487a0e2a5cff0531c3dc0d
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 10:51:39 -0500

    Added route for PageNotFound page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import {Switch, Route} from 'react-router-dom';
 
 // Components
 import LoginPage from './pages/LoginPage'
+import PageNotFound from './pages/PageNotFound'
 
 
 function App() {
   return (
     <Switch>
-      <Route path='/' component={LoginPage} />
+      <Route path='/' component={LoginPage} exact/>
+      <Route component={PageNotFound} exact/>
     </Switch>
   );
 }
