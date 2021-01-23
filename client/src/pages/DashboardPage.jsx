1865846993a0975eb57ddf237037c4a9feebf589
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 11:14:19 -0500

    Created DashboardPage

---
+++ client/src/pages/DashboardPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const DashboardPage = () => {
+    return(
+        <Layout>
+            <div>
+                <h1 className='text-6xl'>
+                    Dashboard Page!
+                </h1>
+            </div>
+        </Layout>
+    )
+}
+
+export default DashboardPage;
