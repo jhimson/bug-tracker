f1243a7210ff8ca27e99a56e1a089d6edf98142b
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 11:08:07 -0500

    Updated RegisterPage design

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import logo from '../assets/images/login-logo.png'
+import { Link } from 'react-router-dom'
+
+// components
 import Layout from '../components/Layout';
 
-import logo from '../assets/images/login-logo.png'
 
 
 
@@ -13,15 +16,17 @@ const RegisterPage = () => {
                         <div>
                             <img src={logo} alt="" className='w-full h-72'/>
                         </div>
-                        <form className='space-y-2'>
+                        <form className='space-y-4'>
                             <h1 className='text-2xl font-bold text-center'>Account Register</h1>
-                            <div>
-                                <label htmlFor="firstname" className='font-semibold'>Firstname:</label>
-                                <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
-                            </div>
-                            <div>
-                                <label htmlFor="lastname" className='font-semibold'>Lastname:</label>
-                                <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                            <div className='flex space-x-5'>
+                                <div>
+                                    <label htmlFor="firstname" className='w-3/6 font-semibold'>Firstname:</label>
+                                    <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                                </div>
+                                <div>
+                                    <label htmlFor="lastname" className='w-3/6 font-semibold'>Lastname:</label>
+                                    <input type="text" className='block w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:border-blue-500'/>
+                                </div>
                             </div>
                             <div>
                                 <label htmlFor="email" className='font-semibold'>Email:</label>
@@ -35,7 +40,7 @@ const RegisterPage = () => {
                                 <button className='w-full py-2 mt-5 text-lg font-bold bg-blue-400 rounded focus:outline-none focus:ring-2 '>Sign up</button>
                             </div>
                             <div>
-                                <p className='mt-5 text-center text-md'>Already have an account? <a href="" className='text-purple-600'>Sign in</a></p>
+                                <p className='mt-5 text-center text-md'>Already have an account? <Link to='/' className='text-purple-600'>Sign in</Link></p>
                             </div>
                         </form>
                     </div>
