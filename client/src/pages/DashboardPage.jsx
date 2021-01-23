3012789c73ae24629c66ca43883f35e4f3885bff
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 11:25:48 -0500

    Updated layout design for dashboard page

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -4,10 +4,26 @@ import Layout from '../components/Layout';
 const DashboardPage = () => {
     return(
         <Layout>
-            <div>
-                <h1 className='text-6xl'>
-                    Dashboard Page!
-                </h1>
+            <div className='h-screen'>
+                <div className='flex items-center justify-center m-10 space-x-5'>
+                    <div className='flex flex-col w-screen space-y-10'>
+                        <div>
+                            <h1 className='text-6xl bg-gray-200'>
+                                Projects Section
+                            </h1>
+                        </div>
+                        <div>
+                            <h1 className='text-6xl bg-gray-200'>
+                                My issues Section
+                            </h1>
+                        </div>
+                    </div>
+                    <div className='w-screen'>
+                        <h1 className='text-6xl bg-gray-200'>
+                            Recent updates
+                        </h1>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
