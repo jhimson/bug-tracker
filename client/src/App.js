586868be9cc206f5ea1231dd8dba2c2b352f4e39
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-23 11:02:40 -0500

    Added register page route

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import {Switch, Route} from 'react-router-dom';
 
 // Components
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import PageNotFound from './pages/PageNotFound'
+
 
 
 function App() {
   return (
     <Switch>
       <Route path='/' component={LoginPage} exact/>
+      <Route path='/register' component={RegisterPage} exact/>
       <Route component={PageNotFound} exact/>
     </Switch>
   );
