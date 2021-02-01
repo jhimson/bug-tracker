642ba43c68375563a2d15a6b01ba8859c7d5cb22
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-01-31 20:49:29 -0500

    Add RecentUpdatesList component

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -59,27 +59,33 @@ const DashboardPage = () => (
                     </div>
                   </div>
                 </div>
-                <div className="table w-full p-3 issues">
-                  <table className="w-full text-center border">
-                    <tr>
-                      <th>Key</th>
-                      <th>Subject</th>
-                      <th>Priority</th>
-                      <th>Status</th>
-                      <th>Due</th>
+                <div className="table w-full p-1 issues">
+                  <table className="w-full p-5 text-center border">
+                    <tr className="p-2 border">
+                      <th className="p-2 border">Key</th>
+                      <th className="p-2 border">Subject</th>
+                      <th className="p-2 border">Priority</th>
+                      <th className="p-2 border">Status</th>
+                      <th className="p-2 border">Due</th>
                     </tr>
-                    <tr>
-                      <td className="text-blue-800">TSYS-2</td>
-                      <td>Login page bug</td>
-                      <td>
-                        <IoMdArrowUp size="1em" color="red" />
+                    <tr className="border">
+                      <td className="p-1 text-blue-800 border">TSYS-2</td>
+                      <td className="p-1 border">Login page bug</td>
+                      <td className="p-1 border ">
+                        <div className="flex items-center justify-center">
+                          <IoMdArrowUp
+                            size="1em"
+                            color="red"
+                            className="text-center"
+                          />
+                        </div>
                       </td>
-                      <td>
-                        <button className="p-1 px-3 text-sm text-white bg-red-400 rounded-full">
+                      <td className="p-1 border">
+                        <button className="p-1 px-5 text-sm text-white bg-red-400 rounded-full">
                           Open
                         </button>
                       </td>
-                      <td>Jan.29</td>
+                      <td className="p-1 border">Jan.29</td>
                     </tr>
                   </table>
                 </div>
@@ -97,56 +103,7 @@ const DashboardPage = () => (
               <h1 className="mb-2 text-lg font-bold">Recent updates</h1>
             </div>
           </div>
-          <div className="bg-white rounded shadow-lg ">
-            <div className="px-6 py-2 border-b border-gray-500">
-              <h1 className="font-semibold text-md">Sat Jan. 23, 2021</h1>
-            </div>
-            <div className="flex justify-between p-2 border-b border-gray-300">
-              <div className="flex p-4 space-x-2">
-                <div className="user-icon">
-                  <FaUserCircle size="3.5em" className="text-gray-500" />
-                </div>
-
-                <div className="space-y-1">
-                  <div className="flex justify-between">
-                    <div className="flex space-x-2">
-                      <h1 className="font-semibold text-gray-500">
-                        Jhimson Ray Pamisa
-                      </h1>
-                      <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
-                        updated
-                      </button>
-                      <p className="font-semibold text-gray-500">the issue</p>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center space-x-2 issue_description">
-                    <h1 className="text-sm font-semibold text-blue-400 bg-blue-50 issue_key">
-                      TSYS-2
-                    </h1>
-                    <h1 className="text-sm font-semibold issue_name">
-                      Login page bug
-                    </h1>
-                  </div>
-                </div>
-              </div>
-
-              <div className="pb-3 pr-3 space-y-2">
-                <div className="pt-4">
-                  <h1 className="font-semibold text-gray-500">12 hours ago</h1>
-                </div>
-                <div className="flex space-x-2">
-                  <button className="p-2 bg-gray-400 rounded-full hover:bg-blue-400 focus:outline-none">
-                    <MdComment className="text-white" />
-                  </button>
-                  <div className="flex items-center px-3 space-x-2 text-yellow-300 bg-gray-400 rounded-full cursor-pointer hover:bg-yellow-400 hover:text-white">
-                    <BsFillStarFill />
-                    <p>1</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+          <div className="bg-white rounded shadow-lg">
             <div className="flex justify-between p-2 border-b border-gray-300">
               <div className="flex p-4 space-x-2">
                 <div className="user-icon">
