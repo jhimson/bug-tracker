9c6f5410adbdc97eb40444b674669004320dfb6a
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-02-05 06:20:23 -0500

    Add RecentUpdatesList component to DashboardPage

--- client/src/components/RecentUpdatesList.jsx
+++ client/src/components/RecentUpdatesList.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 
+// components
+import RecentUpdateItem from './RecentUpdateItem';
+
 const RecentUpdatesList = () => (
   <>
     <div className="px-6 py-2 border-b border-gray-500">
       <h1 className="font-semibold text-md">Sat Jan. 23, 2021</h1>
     </div>
+    <RecentUpdateItem
+      projectKey="TSYS"
+      issueName="Bug profile"
+      userFullname="Jhimson Ray Pamisa"
+      action="posted"
+    />
   </>
 );
 
