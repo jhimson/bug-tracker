f019aaf83a229a2bef9a107daf116086eea273fb
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-02-05 06:12:02 -0500

    Add actionMessage logic

--- client/src/components/RecentUpdateItem.jsx
+++ client/src/components/RecentUpdateItem.jsx
@@ -1,9 +1,98 @@
+/* eslint-disable no-unreachable */
 /* eslint-disable react/button-has-type */
 import React from 'react';
 // Icons
 import { MdComment } from 'react-icons/md';
 import { BsFillStarFill } from 'react-icons/bs';
 import { FaUserCircle } from 'react-icons/fa';
+
+const actionMessage = (action) => {
+  switch (action) {
+    case 'posted':
+      return (
+        <p className="font-semibold text-gray-500">
+          posted a{' '}
+          <span>
+            <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
+              comment
+            </button>
+          </span>{' '}
+          on the issue
+        </p>
+      );
+      break;
+
+    case 'updated':
+      return (
+        <p className="font-semibold text-gray-500">
+          <span>
+            <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
+              updated
+            </button>
+          </span>{' '}
+          the issue
+        </p>
+      );
+      break;
+
+    case 'edited':
+      return (
+        <p className="font-semibold text-gray-500">
+          edited the{' '}
+          <span>
+            <button className="px-2 font-semibold text-white bg-red-500 rounded-full focus:outline-none">
+              members
+            </button>
+          </span>{' '}
+          of this project
+        </p>
+      );
+      break;
+
+    case 'notified':
+      return (
+        <p className="font-semibold text-gray-500">
+          notified{' '}
+          <span>
+            <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
+              comment
+            </button>
+          </span>{' '}
+          on the issue
+        </p>
+      );
+      break;
+
+    case 'addedIssue':
+      return (
+        <p className="font-semibold text-gray-500">
+          added a new{' '}
+          <span>
+            <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
+              issue
+            </button>
+          </span>
+        </p>
+      );
+      break;
+
+    case 'addedVM':
+      return (
+        <p className="font-semibold text-gray-500">
+          added a new{' '}
+          <span>
+            <button className="px-2 font-semibold text-white bg-red-500 rounded-full focus:outline-none">
+              Version/Milestone
+            </button>
+          </span>
+        </p>
+      );
+      break;
+
+    default:
+      return <p>WTF</p>;
+  }
+};
 
 const RecentUpdateItem = ({ projectKey, issueName, userFullname, action }) => (
   <div className="flex justify-between p-2 border-b border-gray-300">
@@ -15,25 +104,16 @@ const RecentUpdateItem = ({ projectKey, issueName, userFullname, action }) => (
       <div className="space-y-1">
         <div className="flex justify-between">
           <div className="flex space-x-2">
-            <h1 className="font-semibold text-gray-500">Jhimson Ray Pamisa</h1>
-
-            <p className="font-semibold text-gray-500">
-              Posted a{' '}
-              <span>
-                <button className="px-2 font-semibold text-white bg-blue-400 rounded-full focus:outline-none">
-                  comment
-                </button>
-              </span>{' '}
-              on the issue
-            </p>
+            <h1 className="mr-1 font-semibold text-gray-500">{userFullname}</h1>
           </div>
+          {actionMessage(action)}
         </div>
 
         <div className="flex items-center space-x-2 issue_description">
           <h1 className="text-sm font-semibold text-blue-400 bg-blue-50 issue_key">
-            TSYS-2
+            {projectKey}
           </h1>
-          <h1 className="text-sm font-semibold issue_name">Login page bug</h1>
+          <h1 className="text-sm font-semibold issue_name">{issueName}</h1>
         </div>
       </div>
     </div>
