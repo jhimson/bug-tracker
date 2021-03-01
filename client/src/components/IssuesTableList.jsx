c69768ff13e8c6bff4fcbc185ae84046b8c0bacc
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-01 05:47:53 -0500

    Separate Issue table items as its stand alone component

--- client/src/components/IssuesTableList.jsx
+++ client/src/components/IssuesTableList.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 
 // icons
-import { IoMdArrowUp } from 'react-icons/io';
+import { IoMdArrowUp, IoMdArrowForward, IoMdArrowDown } from 'react-icons/io';
+import IssueTableItem from './IssueTableItem';
 
 const IssuesTableList = () => (
   <div className="table w-full p-1 issues">
@@ -14,20 +15,50 @@ const IssuesTableList = () => (
         <th className="p-2 border">Status</th>
         <th className="p-2 border">Due</th>
       </tr>
+      <IssueTableItem
+        issueKey="TSYS-2"
+        issueSubject="Login page bug"
+        issuePriority="High"
+        issueStatus="Open"
+        issueDueDate="Jan.29"
+      />
       <tr className="border">
         <td className="p-1 text-blue-800 border">TSYS-2</td>
-        <td className="p-1 border">Login page bug</td>
+        <td className="p-1 font-semibold text-gray-600 border">
+          Login page bug
+        </td>
         <td className="p-1 border ">
           <div className="flex items-center justify-center">
-            <IoMdArrowUp size="1em" color="red" className="text-center" />
+            <IoMdArrowForward
+              size="1.5em"
+              color="green"
+              className="text-center"
+            />
           </div>
         </td>
         <td className="p-1 border">
-          <button className="p-1 px-5 text-sm text-white bg-red-400 rounded-full">
-            Open
+          <button className="p-1 px-5 text-sm text-white bg-green-500 rounded-full">
+            Resolved
           </button>
         </td>
-        <td className="p-1 border">Jan.29</td>
+        <td className="p-1 font-semibold text-gray-600 border">Jan.29</td>
+      </tr>
+      <tr className="border">
+        <td className="p-1 text-blue-800 border">TSYS-2</td>
+        <td className="p-1 font-semibold text-gray-600 border">
+          Login page bug
+        </td>
+        <td className="p-1 border ">
+          <div className="flex items-center justify-center">
+            <IoMdArrowDown size="1.5em" color="blue" className="text-center" />
+          </div>
+        </td>
+        <td className="p-1 border">
+          <button className="p-1 px-5 text-sm text-white bg-blue-500 rounded-full">
+            In Progress
+          </button>
+        </td>
+        <td className="p-1 font-semibold text-gray-600 border">Jan.29</td>
       </tr>
     </table>
   </div>
