2f51f53d3950e80ba740bf1d3d0bb7aeda74ba4e
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-07 15:33:17 -0500

    Update height of footer and positioning of the texts

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    <div className="text-center text-white bg-black">Footer</div>
+    <div className="flex items-center justify-center h-full text-lg text-center text-white bg-black">
+      Bug Tracker Inc. | 2020
+    </div>
   </footer>
 );
 
