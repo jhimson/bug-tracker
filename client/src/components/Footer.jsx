58d693d0b0bd1f9a52d870b1d9df14e682504e67
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-07 15:57:18 -0500

    Update footer text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <div className="flex items-center justify-center h-full text-lg text-center text-white bg-black">
-      Bug Tracker Inc. | 2020
+      <h1>Copyright © 2021 | Bug Tracker</h1>
     </div>
   </footer>
 );
