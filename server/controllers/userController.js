92624028b67bb740582fbefaaa825c01f43ad1ad
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-21 13:00:37 -0400

    Add registerUser route handler in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,12 @@
+/* eslint-disable camelcase */
 const asyncHandler = require("express-async-handler");
 
-const { fetchUsers } = require("../db/models/userModel");
+const {
+  fetchUsers,
+  findEmail,
+  createNewUser,
+} = require("../db/models/userModel");
+const { generateToken } = require("../utils/auth");
 
 // ? @Description    Fetch all users
 // ? @Route          GET /api/v1/users
@@ -15,6 +21,39 @@ const getUsers = asyncHandler(async (req, res) => {
   }
 });
 
+// ? @Description    Add new user
+// ? @Route          POST /api/v1/users
+// ? @Access         Private/Admin
+const registerUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const userExists = await findEmail(req.body.email);
+
+  if (userExists.rowCount) {
+    res.status(400);
+    throw new Error("Email already exists!");
+  }
+
+  const newUser = await createNewUser(req.body);
+
+  const { rows, rowCount } = newUser;
+  const { user_id, firstname, lastname, email, is_admin } = rows[0];
+
+  if (rowCount) {
+    res.status(201).json({
+      user_id,
+      firstname,
+      lastname,
+      email,
+      is_admin,
+      token: generateToken(user_id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Something went wrong! Invalid user data");
+  }
+});
+
 module.exports = {
   getUsers,
+  registerUser,
 };
