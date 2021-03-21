deb52ab75e83eb41c5a229644921991eca0f6ff3
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-21 10:34:35 -0400

    Add findEmail query function in userModel - [server side]

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -2,6 +2,13 @@ const db = require("..");
 
 const fetchUsers = () => db.query("SELECT * FROM tbl_users");
 
+const findEmail = (email) =>
+  db.query({
+    text: `SELECT * FROM tbl_users WHERE email = $1`,
+    values: [email],
+  });
+
 module.exports = {
   fetchUsers,
+  findEmail,
 };
