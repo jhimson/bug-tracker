94926d18c2c8620050931afa708a7f6544db09c2
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-21 09:04:58 -0400

    Fix userRoutes get request to pull all users

--- server/api/userRoutes.js
+++ server/api/userRoutes.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
 const Router = express.Router();
+
+const { getUsers } = require("../controllers/userController");
+
+Router.route("/").get(getUsers);
+
+module.exports = Router;
