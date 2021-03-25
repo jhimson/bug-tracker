d24be3e1e0f06de7d9af71aae981a53505314cc9
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-24 20:40:52 -0400

    Add userActions

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,46 @@
+/* eslint-disable camelcase */
+import Axios from 'axios';
+
+import { userConstants } from '../constants/userConstants';
+
+const { uuid } = require('uuidv4');
+
+const {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+} = userConstants;
+
+export const register = (email, firstname, lastname, password) => async (
+  dispatch
+) => {
+  const user_id = uuid();
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const { data } = await Axios.post(
+      'http://localhost:5000/api/v1/users',
+      { user_id, firstname, lastname, email, password },
+      config
+    );
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+    // dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.error.message
+          ? error.response.data.error.message
+          : error.message,
+    });
+  }
+};
