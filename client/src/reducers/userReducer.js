a667f1dbc1d0f050d4ca81ebec21884b80903042
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-25 08:02:24 -0400

    Add userRegisterReducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,3 +1,22 @@
+export const userRegisterReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case 'USER_REGISTER_REQUEST':
+      return { ...state, loading: true };
+
+    case 'USER_REGISTER_SUCCESS':
+      return { ...state, loading: false, userInfo: payload };
+
+    case 'USER_REGISTER_FAIL':
+      return { ...state, loading: false, error: payload };
+
+    case 'USER_REGISTER_RESET':
+      return { ...state, loading: false, userInfo: {} };
+
+    default:
+      return state;
+  }
+};
+
 export const userLoginReducer = (state = {}, { type, payload }) => {
   switch (type) {
     case 'USER_LOGIN_REQUEST':
