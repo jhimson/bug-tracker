fa19312cbff0f2918c856852c93d2cf016adaed8
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-03-25 21:03:39 -0400

    Update: import constant variable from userConstants

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,16 +1,37 @@
+import { userConstants } from '../constants/userConstants';
+
+const {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_RESET,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  SET_ERROR_TO_NULL,
+} = userConstants;
+
 export const userRegisterReducer = (state = {}, { type, payload }) => {
   switch (type) {
-    case 'USER_REGISTER_REQUEST':
+    case USER_REGISTER_REQUEST:
       return { ...state, loading: true };
 
-    case 'USER_REGISTER_SUCCESS':
-      return { ...state, loading: false, userInfo: payload };
+    case USER_REGISTER_SUCCESS:
+      return { ...state, loading: false, error: null, userInfo: payload };
 
-    case 'USER_REGISTER_FAIL':
+    case USER_REGISTER_FAIL:
       return { ...state, loading: false, error: payload };
 
-    case 'USER_REGISTER_RESET':
+    case USER_REGISTER_RESET:
       return { ...state, loading: false, userInfo: {} };
+
+    case SET_ERROR_TO_NULL:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+      };
 
     default:
       return state;
@@ -19,16 +40,16 @@ export const userRegisterReducer = (state = {}, { type, payload }) => {
 
 export const userLoginReducer = (state = {}, { type, payload }) => {
   switch (type) {
-    case 'USER_LOGIN_REQUEST':
+    case USER_LOGIN_REQUEST:
       return { ...state, loading: true };
 
-    case 'USER_LOGIN_SUCCESS':
+    case USER_LOGIN_SUCCESS:
       return { ...state, loading: false, userInfo: payload };
 
-    case 'USER_LOGIN_FAIL':
+    case USER_LOGIN_FAIL:
       return { ...state, loading: false, error: payload };
 
-    case 'USER_LOGOUT':
+    case USER_LOGOUT:
       return { ...state, userInfo: null };
 
     default:
